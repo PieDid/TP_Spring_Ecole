817e417b781d2d737d7e0279ac8df156f1670951
Cheng Jun <d.cheng@live.fr>
2020-04-30 21:45:04 +0200

    Didier : Ajout de AdresseDao, Controller et Validator ; MaJ des validations

--- TP_Spring_GestionEcole/src/main/java/com/intiformation/gestionecole/dao/MatiereDao.java
+++ TP_Spring_GestionEcole/src/main/java/com/intiformation/gestionecole/dao/MatiereDao.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.intiformation.gestionecole.domain.Matiere;
 
+@Transactional
+@Repository
 public class MatiereDao implements IMatiereDao {
 	
 	@Autowired
